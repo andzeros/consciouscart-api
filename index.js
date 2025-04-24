d520dad043eb6933bf173428d5b6d9a8676cf150
andzeros <writeandzeros@gmail.com>
2025-04-23 20:36:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import OpenAI from 'openai';
 
 const app = express();
